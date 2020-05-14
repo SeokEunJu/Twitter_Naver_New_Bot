2553ae9f810f6c89341e18c7e9172bbcdcfc01e7
SeokEunJu <solya@g.skku.edu>
2020-05-14 22:41:07 +0900

    naver.py 기능별로 나눠서 함수 만들기

--- naver.py
+++ naver.py
@@ -4,49 +4,93 @@ import os
 import sys
 import json
 
-client_id='Your_client_id'
-client_secret='Your_client_secret'
+client_id=os.environ['NAVER_CLIENT_ID']
+client_secret=os.environ['NAVER_CLIENT_SECRET']
+ 
+result_title=[]
 result_url=[]
 result_short_url=[]
-params={'query':'남양','display':'3'}
-search_keyword=urllib.parse.quote("남양")
-display_num="&display=3"
-url="https://openapi.naver.com/v1/search/news?query="+search_keyword+display_num
-#json 결과
-# url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
 
-request=urllib.request.Request(url)
-request.add_header("X-Naver-Client-Id",client_id)
-request.add_header("X-Naver-Client-Secret",client_secret)
-response=urllib.request.urlopen(request)
-rescode=response.getcode()
-if(rescode==200):
-    response_body=response.read()
-#print(response_body.decode('utf-8'))
-#print(type(response_body))->byte
-#print(type(response_body.decode('utf-8')))->str
-    result_json=response_body.decode('utf-8')
-    dict=json.loads(result_json)
-#originallink
-    for h in dict['items']:
-        result_url.append(h['originallink'])
-else:
-    print("Error Code:" + rescode)
+#make NAVER_API formatted url
+#*args는 모든 input에 대해 같은 동작을 할 때 적합한 거 같아요
+#이 함수의 인자 개수는 2개, 4개로 들어옵니다. 총 2가지 경우에요
+#처음 두 인자는 지금 무슨 API를 호출한 건지 말해주고
+#나머지 두 인자는 url에 넣을 keyword라서
+#최소 2개의 인자를 받고 그 후에 들어오는 것은 *args로 처리해주었습니다!
+def makeurl(API_type, API_name,*args):
+    #news search
+    if(API_type=="search"):
+        if(API_name=="news"):
+            search_keyword=urllib.parse.quote(args[0])
+            display_num="&display="+str(args[1])
+            #request json type result
+            url="https://openapi.naver.com/v1/"+API_type+"/"+API_name+"?query="+search_keyword+display_num+"&sort=sim"
+            return url
+    #util shorturl
+    if(API_type=="util"):
+    	if(API_name=="shorturl"):
+    	    url="https://openapi.naver.com/v1/"+API_type+"/"+API_name
+    	    return url
+    #wrong input format
+    return 0
 
-for source in result_url:
-    source_url = urllib.parse.quote(source)
-    data="url="+source_url
-    url = "https://openapi.naver.com/v1/util/shorturl"
-    request = urllib.request.Request(url)
+#attatch id and secret key to reauest header
+def get_request(url):
+    request=urllib.request.Request(url)
     request.add_header("X-Naver-Client-Id",client_id)
     request.add_header("X-Naver-Client-Secret",client_secret)
-    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
-    rescode = response.getcode()
+    return request
+
+#get three titles and news-links
+def news_links(request):
+    response=urllib.request.urlopen(request)
+    rescode=response.getcode()
     if(rescode==200):
         response_body=response.read()
         result_json=response_body.decode('utf-8')
         dict=json.loads(result_json)
-        result_short_url.append(dict['result']['url'])
-    else:
-        print("Error Code:" + rescode)
-print(result_short_url)
+        #originallink
+        for h in dict['items']:
+            result_title.append(h['title'])
+            result_url.append(h['originallink'])
+    #return result code. 200 is success! else failed.
+    return rescode
+
+#long url->short url
+def make_short_url(result_url):
+    for source in result_url:
+        source_url = urllib.parse.quote(source)
+        data="url="+source_url
+        url = makeurl("util","shorturl")
+        request=get_request(url)
+        response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+        rescode = response.getcode()
+        if(rescode==200):
+            response_body=response.read()
+            result_json=response_body.decode('utf-8')
+            dict=json.loads(result_json)
+            result_short_url.append(dict['result']['url'])
+        else:
+            print("Error Code:" + rescode)
+
+def main():
+    url=makeurl("search","news","코로나", 3)
+    if(url==0):
+        print("wrong input format")
+        return
+    request=get_request(url)
+    rescode=news_links(request)
+    if(rescode!=200):
+        print("failed to get news links")
+        return
+    make_short_url(result_url)
+    print(result_title)
+    print(result_short_url)
+    #지금의 코드는 그냥 한번 키워드 검색하고 끝나지만 나중엔 계속계속 호출을 할 거니까
+    #전역변수로 설정된 list들을 초기화 시켜주었어요
+    result_title.clear()
+    result_url.clear()
+    result_short_url.clear()
+    
+if __name__ == "__main__":
+    main()
--- twitter.py
+++ twitter.py
@@ -1,18 +1,24 @@
 import tweepy
+import os
 #Hello!
-consumer_key='Your_consumer_key'
-consumer_secret='Your_consumer_secret'
-access_token='Your_access_token'
-access_token_secret='Your_access_token_secret'
+consumer_key=os.environ['TWITTER_CONSUMER_KEY']
+consumer_secret=os.environ['TWITTER_CONSUMER_SECRET']
+access_token=os.environ['TWITTER_ACCESS_TOKEN']
+access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
 auth=tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 api=tweepy.API(auth)
 
-tweet="Hello It's me"
-api.update_status(status=tweet)
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+    tweet="Hello It's me"
+    api.update_status("이거 좀 올려줘...")
+except:
+    print("Error during authentication")
 """
-user=api.get_user("solya_xd")
+user=api.get_user("naver_news_bot")
 
 print("User details: ")
 print(user.name)
@@ -21,5 +27,5 @@ print(user.location)
 print("Last 20 Followers:")
 for follower in user.followers():
     print(follower.name)
-"""
 
+"""
